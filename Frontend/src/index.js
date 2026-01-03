59b6f3dbdb83ef54f87053157c5da5a907f8adc3
kushal-cloud <kushalj592@gmail.com>
2026-01-03 23:11:10 +0530

    Commit

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// You can create an empty index.css file if you don't have one yet
 import './index.css'; 
 
+// valid React 18+ initialization
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
